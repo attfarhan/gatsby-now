cd7863a587cbb4394a713c208b83ba204415ac41
Farhan Attamimi <farhan@sourcegraph.com>
2018-09-04 15:35:35 -0700

    2

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,7 +7,7 @@ const IndexPage = () => (
   <Layout>
     <h1>Hi people</h1>
     <p>Welcome to this site deployed with Now.</p>
-    <p>Now go build something great.</p>
+    <p>Now go build something great 2.</p>
     <Link to="/page-2/">Go to page 2</Link>
   </Layout>
 )
