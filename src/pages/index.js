4bcd72d78c7bcefe491ec2451823612f4973b81e
Farhan Attamimi <farhan@sourcegraph.com>
2018-09-04 14:33:42 -0700

    More changes

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,7 +7,7 @@ const IndexPage = () => (
   <Layout>
     <h1>Hi people</h1>
     <p>Welcome to Farhan's Gatsby Now site.</p>
-    <p>Now go build something great.</p>
+    <p>Now pull requests should auto-deploy to a now domain.</p>
     <Link to="/page-2/">Go to page 2</Link>
   </Layout>
 )
