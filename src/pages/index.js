9d3e03186c93410061f76d56857ce7d3c3f75054
Farhan Attamimi <farhan@sourcegraph.com>
2018-09-04 15:51:19 -0700

    Update

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,7 +7,7 @@ const IndexPage = () => (
   <Layout>
     <h1>Hi people</h1>
     <p>Welcome to this site deployed with Now.</p>
-    <p>Now go build something great.</p>
+    <p>This is now.attamimi.org.</p>
     <Link to="/page-2/">Go to page 2</Link>
   </Layout>
 )
